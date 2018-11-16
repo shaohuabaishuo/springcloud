cb7fc840516701f6d2c723f58bfe656a0bbbaccf
jingzhong <chunyangwanhua>
2018-11-16 13:41:19 +0800

    springcloud完整demo

--- eurekserver/src/main/java/com/example/eurekserver/EurekserverApplication.java
+++ eurekserver/src/main/java/com/example/eurekserver/EurekserverApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 
 @SpringBootApplication
 @EnableEurekaServer
--- serviceturbine/src/main/java/com/example/serviceturbine/ServiceturbineApplication.java
+++ serviceturbine/src/main/java/com/example/serviceturbine/ServiceturbineApplication.java
@@ -1,22 +1,26 @@
-package com.example.servicefeign;
+package com.example.serviceturbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableFeignClients
+@RestController
 @EnableHystrix
 @EnableHystrixDashboard
-public class ServicefeignApplication {
+@EnableCircuitBreaker
+@EnableTurbine
+public class ServiceturbineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServicefeignApplication.class, args);
+        SpringApplication.run(ServiceturbineApplication.class, args);
     }
 }
